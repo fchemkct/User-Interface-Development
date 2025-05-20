5df1485db93c9966d6d60e5a8b43dc59fbcfa2e9
fchemkct <hieu24a4@gmail.com>
2025-05-20 15:20:54 +1000

    feat:dropdown menu pushes below content downwards

---
+++ Css_stylesheet/testjs.js
@@ -0,0 +1,5 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById('dropdownOptions');
+    dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
+  }
+  
