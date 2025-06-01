a10a7dfd50101f326225850aaef1adf87fc919c1
fchemkct <hieu24a4@gmail.com>
2025-06-01 22:24:30 +1000

    fix: refine search bar appearance

--- docs/Javascript/search_engine.js
+++ docs/Javascript/search_engine.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const search_input = localStorage.getItem("search_input");
     const products = document.querySelectorAll("div.product");
     const resultContainer = document.getElementById("product-grid");
+    const searchCount = document.getElementById("search-count");
+    var count = 0;
+
     resultContainer.innerHTML = "";
 
     let search_matches = [];
@@ -20,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const lowerQuery = search_input.toLowerCase();
     console.log("filtered input is: " + lowerQuery);
 
+    // find matches with input from database
     products.forEach(product => 
     {
         const text = product.querySelector("a").textContent.toLowerCase();
@@ -28,16 +32,27 @@ document.addEventListener("DOMContentLoaded", () => {
         if (text.includes(lowerQuery)) 
         {
             search_matches.push(product.cloneNode(true));
+            count++;
         } 
     });
 
     // if no searches matched
     if (search_matches.length == 0)
     {
-        resultContainer.innerHTML = `<p style="text-align:center; font-family:cursive;">No results found for "${search_input}"</p>`
+        searchCount.innerHTML = "";
+        resultContainer.innerHTML = `<p style="text-align:center; font-family:cursive;">No results found for "${search_input}"</p>`;
     }
         
     // Insert results in groups of 4 per grid
+    if (count == 1) 
+    {
+        searchCount.innerHTML = count + " result";
+    }
+
+    if (count > 1)
+    {
+        searchCount.innerHTML = count + " results";
+    }
     for (let i = 0; i < search_matches.length; i += 4) {
         const grid = document.createElement("div");
         grid.classList.add("product-grid");
